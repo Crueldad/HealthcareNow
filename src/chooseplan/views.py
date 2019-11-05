20acfb113ec594ef74462bfcbaa20460cec62bb9
crueldad <ian.crueldad@cgu.edu>
2019-11-05 14:12:26 -0800

    Added forms and made changes to chooseplan.html file

--- src/chooseplan/views.py
+++ src/chooseplan/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from chooseplan.forms import chooseplanform
 
 
 def chooseplan(request):
     return render(request, 'chooseplan/chooseplan.html')
+
+def get(self, request):
+    form = chooseplanform()
+    return render(request, self.chooseplan.html, {'form': form})
