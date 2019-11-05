c76179970bc93c0390a0d214e7cfa2cd481f4aeb
crueldad <ian.crueldad@cgu.edu>
2019-11-05 11:28:36 -0800

    Made database model migrations

--- src/chooseplan/admin.py
+++ src/chooseplan/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Healthcare_Categories
+
+admin.site.register(Healthcare_Categories)
--- src/chooseplan/models.py
+++ src/chooseplan/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Healthcare_Categories(models.Model):
+    UserID = models.IntegerField(blank=True, primary_key=True)
+    UserName = models.CharField(max_length=50, blank=True)
+    UserEmail = models.CharField(max_length=50, blank=True)
+    UserPassword = models.CharField(max_length=50, blank=True)
+    SpecialistServices = models.IntegerField(blank=True)
+    PreventativeCare = models.IntegerField(blank=True)
+    DiagnosticTest = models.IntegerField(blank=True)
+    GenericDrugs = models.IntegerField(blank=True)
+    OutpatientSurgery = models.IntegerField(blank=True)
+    ImmediateMedicalAttention = models.IntegerField(blank=True)
+    OutpatientInpatient = models.IntegerField(blank=True)
+    Pregnancy = models.IntegerField(blank=True)
+    HomeHealthCare = models.IntegerField(blank=True)
+    RehabilationServices = models.IntegerField(blank=True)
+    SkilledNursingCare = models.IntegerField(blank=True)
